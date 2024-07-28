3cc7271ed4dcb01b966fbd53fffc944a3b5c16bf
dishizawa <dishizawa2001@gmail.com>
2024-07-28 16:17:50 +0900

    ConvertFromStringにおけるエラーの扱いをpanicから返り値で返す方式へ変更

--- api/libs/math/big_int/big_int_conversions.go
+++ api/libs/math/big_int/big_int_conversions.go
@@ -1,11 +1,29 @@
 package bigint
 
-import "math/big"
+import (
+	"fmt"
+	"math/big"
+)
 
-func ConvertFromString(s string) *big.Int {
-	bigNum, ok := new(big.Int).SetString(s, 10)
-	if !ok {
-		panic("failed to parse big integer")
-	}
-	return bigNum
+// ConvertFromString
+// String型の整数をbig.Intに変換する関数
+//
+// Args:
+//		numeric_string (string) : String型の整数
+//
+// Returns:
+//		bigNum (*big.Int) : big.Int型の整数
+//		err (error) : エラー
+// Errorf:
+//		"failed to parse big integer: %s": numericStringがbig.Intに変換できない文字列だった
+func ConvertFromString(numericString string) (*big.Int, error) {
+	// numericStringをbig.Int型の値bigNumへ変換
+    bigNum, ok := new(big.Int).SetString(numericString, 10)
+
+	// 正常に変換が行えたかを確認
+    if !ok {
+        return nil, fmt.Errorf("failed to parse big integer: %s", numericString)
+    }
+	
+    return bigNum, nil
 }
