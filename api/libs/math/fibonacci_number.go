ac82d9d3433842ab14dbd31446e4236849036965
dishizawa <dishizawa2001@gmail.com>
2024-07-28 14:58:45 +0900

    フィボナッチ数の計算にbig.Intを使用するように変更

--- api/libs/math/fibonacci_number.go
+++ api/libs/math/fibonacci_number.go
@@ -2,6 +2,8 @@ package fibonacci
 
 import (
 	"fmt"
+
+	"math/big"
 )
 
 // GenerateFibonacciNumber
@@ -15,21 +17,30 @@ import (
 //
 //	fibonacciNumber (int) : 指定された番目のフィボナッチ数.fibonacci_index = 0 の返り値は0.
 //	err (error) : エラー
-func GenerateFibonacciNumber(index int16) (fibonacciNumber int, err error) {
+func GenerateFibonacciNumber(index int16) (fibonacciNumber *big.Int, err error) {
 	if index > 0 {
-		fibonacciNumber := 1
-		nextValue := 1
+		
+		// フィボナッチ数を big.Int で計算
+		fibonacciNumber = big.NewInt(1)
+		nextValue := big.NewInt(1)
+		tmp := big.NewInt(0) // 一時的な計算結果を格納する変数
 
 		for i := int16(1); i < index; i++ {
-			fibonacciNumber, nextValue = nextValue, nextValue+fibonacciNumber
+			// fibonacciNumber + nextValue を tmp に保存
+			tmp.Add(fibonacciNumber, nextValue)
+			// nextValue を fibonacciNumber に設定
+			fibonacciNumber.Set(nextValue)
+			// tmp を nextValue に設定
+			nextValue.Set(tmp)
 		}
+
 
 		return fibonacciNumber, nil
 
 	} else if index < 0 {
-		return 0, fmt.Errorf("invalid index: %d", index)
+		return big.NewInt(0), fmt.Errorf("invalid index: %d", index)
 
 	} else {
-		return 0, nil
+		return big.NewInt(0), nil
 	}
 }
